b79f9d456ecb127096a623cf95e68205a618fa05
Code Ape <fardin.barashi@gmail.com>
2024-07-31 10:10:04 +0200

    Update Password ConnectToServerUsingCryptTextfile.ps1

--- Powershell/Snippets/Password ConnectToServerUsingCryptTextfile.ps1
+++ Powershell/Snippets/Password ConnectToServerUsingCryptTextfile.ps1
@@ -34,8 +34,8 @@ Write-Host ".. Starting TranScript"
 # Error-Settings
 $ErrorActionPreference = 'Continue'
 #----------------------------------- Start Script ------------------------------------------
-# Section 1 : Create and save crypt password in a textfile
-$Section = "Section 1 : Create and save crypt password in a textfile"
+# Section 1 : decrypt password in a textfile, connect to server
+$Section = "Section 1 : decrypt password in a textfile, connect to server"
 Try
 { # Start Try, $Section
  Get-Date -Format "yyyy/MM/dd HH:mm:ss"
