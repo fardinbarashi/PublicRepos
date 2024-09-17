5f9a9d4b1ef15832bdd2b0a6491d622abecbe940
Code Ape <fardin.barashi@gmail.com>
2024-09-17 17:56:05 +0200

    Add files via upload

---
+++ Powershell/Snippets/Check if string is null.ps1
@@ -0,0 +1,12 @@
+﻿# Check if string is null
+
+$StringToCheck = ""
+  
+  If([string]::IsNullOrEmpty($StringToCheck))
+      { 
+       String is empty!
+      }
+Else 
+ { 
+  String is Not empty
+ }
