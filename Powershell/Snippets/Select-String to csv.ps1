671661a86370ac66a07c1e7644c2b85e300222bb
Code Ape <fardin.barashi@gmail.com>
2025-04-14 10:19:31 +0200

    Add files via upload

---
+++ Powershell/Snippets/Select-String to csv.ps1
@@ -0,0 +1,17 @@
+﻿$FilePath = ""
+$Keywords = "Koroma"
+
+
+$GetItems = Get-ChildItem -Path $FilePath -Recurse -Force
+
+
+$Results = $GetItems | Select-String -Pattern $Keywords | ForEach-Object {
+    [PSCustomObject]@{
+        Filename   = $_.Path
+        LineNumber = $_.LineNumber
+        LineText   = $_.Line
+    }
+}
+
+
+$Results | Export-Csv "$PSScriptRoot\SearchAfter_$($Keywords).csv" -Force -Encoding UTF8 -NoTypeInformation
