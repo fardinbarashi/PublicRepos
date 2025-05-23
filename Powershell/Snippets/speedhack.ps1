ac4a463a07c73a638ae6438fb2574ffcc4d3922c
Code Ape <fardin.barashi@gmail.com>
2025-05-23 13:33:25 +0200

    Add files via upload

---
+++ Powershell/Snippets/speedhack.ps1
@@ -0,0 +1,30 @@
+﻿$Time = Measure-Command -Expression {
+  $Files = Get-ChildItem -Path 'C:\Program Files' -Recurse -ErrorAction SilentlyContinue
+
+  # Initialize an empty array
+  $LastWriteTime = @()
+
+  # Iterate through each file and add its LastWriteTime to the array
+  foreach($File in $Files)
+  {
+    $LastWriteTime += $File.LastWriteTime  # Adding to the array using += is inefficient
+  }
+}
+
+$Time.TotalSeconds  # Outputs the time taken for execution
+
+
+$Time = Measure-Command -Expression {
+  $Files = Get-ChildItem -Path 'C:\Program Files' -Recurse -ErrorAction SilentlyContinue
+
+  # Initialize an empty array
+  $LastWriteTime = @()
+
+  # Use foreach to directly populate the array with values
+  $LastWriteTime = foreach($File in $Files)
+  {
+    $File.LastWriteTime  # Directly output the LastWriteTime for each file
+  }
+}
+
+$Time.TotalSeconds  # Outputs the time taken for execution
